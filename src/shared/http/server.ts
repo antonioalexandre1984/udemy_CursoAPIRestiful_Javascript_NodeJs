26e58d14d96753e0741b871bb9934715a27d4931
antonioalexandre1984 <53489752+antonioalexandre1984@users.noreply.github.com>
2021-11-25 14:06:48 -0300

    Projeto até a aula 65 - Controller de Redifinição de senha

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,17 +1,22 @@
 import 'reflect-metadata';
-import express, { NextFunction, Response, Request } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
+app.use(errors());
 app.use(
     (
         error: Error,
@@ -31,7 +36,6 @@ app.use(
         });
     },
 );
-
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+    console.log('Server started on port 3333! 🏆');
 });
